f61d2626d51fc744e9b7ac0675c612bf787c1f65
Phil McCown <pjlmac@gmail.com>
2017-09-28 18:28:22 -0400

    Had to run to a different event; here's what I have so far, back at ~9

--- Exercise5b.py
+++ Exercise5b.py
@@ -2,5 +2,8 @@ import pandas
 wages=pandas.read_csv("wages.csv")
 wages.drop([col for col in wages.columns if 'yearsSchool' in col],axis=1,inplace=True)
 a=(wages.sort_values(by='wage'))
+print ("Lowest earner")
 print (a.head(n=1))
+print ("Highest Earner")
 print (a.tail(n=1))
+b=(wages.sort_values(by='gender','wage',ascending=False))
