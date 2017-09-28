9628dc44c64c5468af88c67c3ea6fa906e567929
Phil McCown <pjlmac@gmail.com>
2017-09-28 18:18:48 -0400

    Figured out how to trim out the yearsSchool column

--- Exercise5b.py
+++ Exercise5b.py
@@ -1,5 +1,6 @@
 import pandas
 wages=pandas.read_csv("wages.csv")
+wages.drop([col for col in wages.columns if 'yearsSchool' in col],axis=1,inplace=True)
 a=(wages.sort_values(by='wage'))
 print (a.head(n=1))
 print (a.tail(n=1))
