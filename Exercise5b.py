13603662e18ce024ac7ee2d4ad1a0d923aa84fba
Phil McCown <pjlmac@gmail.com>
2017-09-28 22:22:22 -0400

    Exercise 5b is done! Now only to work on 5c!

--- Exercise5b.py
+++ Exercise5b.py
@@ -1,9 +1,17 @@
 import pandas
 wages=pandas.read_csv("wages.csv")
 wages.drop([col for col in wages.columns if 'yearsSchool' in col],axis=1,inplace=True)
-a=(wages.sort_values(by='wage'))
+cc=(wages.sort_values(by='wage'))
 print ("Lowest earner")
-print (a.head(n=1))
+print (cc.head(n=1))
 print ("Highest Earner")
-print (a.tail(n=1))
-b=(wages.sort_values(by='wage',ascending=False))
+print (cc.tail(n=1))
+print ("Top 10 Female Earners")
+d=(wages.sort_values(by='gender'))
+gender=line[0]
+exp=line[1]
+wage=line[3]
+if 'gender'=='female':
+    a=gender+' '+exp+' '+wage+' '+'\n'
+print (a.tail(n=10))
+    
