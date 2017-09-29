66bad362e0444b09501b6cdc0e0e5d854489448e
Phil McCown <pjlmac@gmail.com>
2017-09-29 08:48:51 -0400

    The Final Answer for Part 2; I'll have part 3 along shortly

--- Exercise5b.py
+++ Exercise5b.py
@@ -2,16 +2,10 @@ import pandas
 wages=pandas.read_csv("wages.csv")
 wages.drop([col for col in wages.columns if 'yearsSchool' in col],axis=1,inplace=True)
 cc=(wages.sort_values(by='wage'))
-print ("Lowest earner")
+print ("Lowest Earner")
 print (cc.head(n=1))
 print ("Highest Earner")
 print (cc.tail(n=1))
 print ("Top 10 Female Earners")
-d=(wages.sort_values(by='gender'))
-gender=line[0]
-exp=line[1]
-wage=line[3]
-if 'gender'=='female':
-    a=gender+' '+exp+' '+wage+' '+'\n'
-print (a.tail(n=10))
-    
+numoffemales=wages[wages['wage']>=wages['wage'].nlargest(10).iloc[-1]]['gender'].eq('female').sum()
+print (numoffemales)
